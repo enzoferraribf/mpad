94b1fc6629b7e78b34b719a1f3c5c784a5cfe85d
Enzo Ferrari <ferrarienzobf@gmail.com>
2024-01-20 11:00:50 -0300

    add gimmick

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <div className="flex h-full w-full items-center justify-center bg-[#1e1e1e] p-3 text-center">
-            <h2>Mpad 👀</h2>
+            <h2 className="background-animate bg-gradient-to-r  from-purple-600 via-sky-600 to-blue-600 p-1">Mpad</h2>
         </div>
     );
 }
