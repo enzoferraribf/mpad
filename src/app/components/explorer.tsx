590b39fe930ab513ce16efad5b87cb1ee0f2b941
Enzo Ferrari <ferrarienzobf@gmail.com>
2024-06-08 11:00:33 +0100

    fix explorer ui

--- src/app/components/explorer.tsx
+++ src/app/components/explorer.tsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 
 import { useRouter } from 'next/navigation';
 
-import { Badge } from 'lucide-react';
+import { Badge } from '@/app/components/badge';
 
 import { ApplicationContext } from '@/app/context/context';
 
@@ -20,25 +20,27 @@ export function Explorer({ related }: { related: string[] }) {
             <CommandInput placeholder="Search for a Pad..." />
 
             <CommandGroup heading="Pads">
-                {related.map((related, index) => {
-                    const paths = related.split('/').filter(path => path);
-                    const lastPath = paths.pop();
+                <div className="max-h-[36rem] max-w-fit overflow-y-scroll">
+                    {related.map((related, index) => {
+                        const paths = related.split('/').filter(path => path);
+                        const lastPath = paths.pop();
 
-                    return (
-                        <CommandItem key={index} onSelect={() => handleNavigation(related)}>
-                            <div key={index} className="flex flex-row space-x-2 truncate text-sm">
-                                {/* Search seems to be based on inner elements. I added this hidden span so the search works with the full path */}
-                                <span hidden>{related}</span>
+                        return (
+                            <CommandItem key={index} onSelect={() => handleNavigation(related)}>
+                                <div key={index} className="flex flex-row space-x-2 text-nowrap text-sm">
+                                    {/* Search seems to be based on inner elements. I added this hidden span so the search works with the full path */}
+                                    <span hidden>{related}</span>
 
-                                {paths.map((path, index) => (
-                                    <Badge key={index}>{path}</Badge>
-                                ))}
+                                    {paths.map((path, index) => (
+                                        <Badge key={index}>{path}</Badge>
+                                    ))}
 
-                                <p key={index}>{lastPath}</p>
-                            </div>
-                        </CommandItem>
-                    );
-                })}
+                                    <p key={index}>{decodeURIComponent(lastPath!)}</p>
+                                </div>
+                            </CommandItem>
+                        );
+                    })}
+                </div>
             </CommandGroup>
         </CommandDialog>
     );
