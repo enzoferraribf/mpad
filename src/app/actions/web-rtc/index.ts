1830bc5e634cc04a939af64cc2a7b2caef59ea99
Enzo Ferrari <ferrarienzobf@gmail.com>
2024-06-10 18:41:55 +0100

    backup ice servers

--- src/app/actions/web-rtc/index.ts
+++ src/app/actions/web-rtc/index.ts
@@ -7,5 +7,14 @@ export async function getICEServers() {
 
     const configuration = await response.json();
 
-    return configuration ?? null;
+    return (
+        [
+            ...configuration,
+            { urls: 'stun:stun.l.google.com:19302' },
+            { urls: 'stun:stun1.l.google.com:19302' },
+            { urls: 'stun:stun2.l.google.com:19302' },
+            { urls: 'stun:stun3.l.google.com:19302' },
+            { urls: 'stun:stun4.l.google.com:19302' },
+        ] ?? null
+    );
 }
